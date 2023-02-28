7eef3bce6b2e601878f993282e4a52e752721c68
jin <jin@hyoo.ru>
2023-03-01 02:34:07 +0300

    Simplified api

--- translate/translate.ts
+++ translate/translate.ts
@@ -1,22 +1,7 @@
 namespace $ {
 	
-	export let $hyoo_lingua_translate_api = 'https://hyoo-translate.hf.space/run/translate'
-	
-	const Response = $mol_data_record({
-		data: $mol_data_array( $mol_data_string )
-	})
-	
-	export function $hyoo_lingua_translate( lang: string, text: string ) {
-			
-		const response = $mol_fetch.json( $hyoo_lingua_translate_api, {
-			method: 'post',
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				data: [ lang, text ]
-			})
-		} )
-		
-		return Response( response as any ).data[0]
+	export function $hyoo_lingua_translate( this: $, lang: string, text: string ) {
+		return this.$mol_huggingface_run( 'hyoo-translate', 0, lang, text )[0]
 	}
 	
 }
