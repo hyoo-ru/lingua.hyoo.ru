e42369e0e6e28aa92c840476bb2718afc9bd8fbb
nin-jin <sairi-na-tenshi@ya.ru>
2023-04-09 13:24:35 +0000

    +Cache

--- translate/translate.ts
+++ translate/translate.ts
@@ -1,7 +1,13 @@
 namespace $ {
 	
 	export function $hyoo_lingua_translate( this: $, lang: string, text: string ) {
-		return this.$mol_huggingface_run( 'hyoo-translate', 0, lang, text )[0]
+		
+		const cache_key = `$hyoo_lingua_translate(${ JSON.stringify(lang) },${ JSON.stringify(text) })`
+		const cached = this.$mol_state_local.value( cache_key )
+		if( cached ) return String( cached )
+		
+		const translated = this.$mol_huggingface_run( 'hyoo-translate', 0, lang, text )[0] as string
+		return this.$mol_state_local.value( cache_key, translated )!
 	}
 	
 }
