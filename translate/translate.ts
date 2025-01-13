97b97af973ef3c150fb4145d0f21f92e951d9b21
jin <jin@hyoo.ru>
2025-01-13 03:14:36 +0300

    Use rapidapi for translate

--- translate/translate.ts
+++ translate/translate.ts
@@ -1,15 +1,33 @@
 namespace $ {
 	
+	const Response = $mol_data_record({
+		trans: $mol_data_string,
+		source_language: $mol_data_string,
+		source_language_code: $mol_data_string,
+		trust_level: $mol_data_number,
+	})
+	
 	export function $hyoo_lingua_translate( this: $, lang: string, text: string ) {
 		
 		if( !text.trim() ) return ''
 		
-		const cache_key = `$hyoo_lingua_translate(${ JSON.stringify(lang) },${ JSON.stringify(text) })`
-		const cached = this.$mol_state_local.value( cache_key )
-		if( cached ) return String( cached )
+		const res = this.$mol_fetch.json(
+			`https://google-translate113.p.rapidapi.com/api/v1/translator/text`,
+			{
+				method: 'POST',
+				headers: {
+					'x-rapidapi-key': 'ac9e15b3ffmsh0ca1100d872cde4p10d0a6jsn6d36584cc6c9',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					from: 'auto',
+					to: lang,
+					text,
+				}),
+			},
+		)
 		
-		const translated = this.$mol_huggingface_run( 'hyoo-translate', 0, lang, text )[0] as string
-		return this.$mol_state_local.value( cache_key, translated )!
+		return Response( res as any ).trans
 	}
 	
 }
