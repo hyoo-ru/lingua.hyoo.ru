aedc23191308f5d111552806185abaa96a625d72
jin <jin+git@hyoo.ru>
2025-01-18 13:47:52 +0300

    better api

--- app/app.view.ts
+++ app/app.view.ts
@@ -29,14 +29,14 @@ namespace $.$$ {
 		
 		@ $mol_action
 		native_translate() {
-			this.$.$mol_state_arg.go({})
+			this.$.$mol_state_arg.clone()
 			this.foreign_text( this.$.$hyoo_lingua_translate( this.foreign_lang(), this.native_text() ) )
 			this.Foreign_pane().dom_node().scrollIntoView({ behavior: 'smooth' })
 		}
 		
 		@ $mol_action
 		foreign_translate() {
-			this.$.$mol_state_arg.go({})
+			this.$.$mol_state_arg.clone()
 			this.native_text( this.$.$hyoo_lingua_translate( this.native_lang(), this.foreign_text() ) )
 			this.Native_pane().dom_node().scrollIntoView({ behavior: 'smooth' })
 		}
