198ac05fee92795045b71c1d7cace24b4445c19d
nin-jin <nin-jin@users.noreply.github.com>
2025-05-10 07:09:45 +0000

    Deploying to gh-pages from @ hyoo-ru/lingua.hyoo.ru@ea526ca0405794b20903129d4952ad840dda7699 🚀

--- web.js
+++ web.js
@@ -9400,7 +9400,9 @@ var $;
             console.warn('Service Worker is not supported.');
         }
         else {
-            navigator.serviceWorker.register('web.js').then(reg => {
+            $mol_dom.addEventListener('DOMContentLoaded', () => {
+                navigator.serviceWorker.register('web.js').then(reg => {
+                });
             });
         }
     }
